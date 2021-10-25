53473550cab5e3da063232492f500c9b3b0d0a9d
notblank00 <pyromancy00@gmail.com>
2021-10-26 01:54:34 +0300

    5 src is ready, no tests tho

--- lab5.rb
+++ lab5.rb
@@ -2,25 +2,23 @@ def calculate_expression x, y, z
     (1 + Math.cos(y - 2)) / (x**4 / 2 + Math.sin(z)**2)
 end
 
-def input_and_correct_strings
-    print "Number of lines: "
+def correct_strings
+    print "Number of strings: "
     n = gets.to_i
-    result = []
     n.times do | line |
-        new_string = ''
-        puts "[" + (line + 1).to_s + "] Your string"
-        gets.chomp.rstrip.split.each_with_index do | word, index |
-            new_string += word + ' ' unless (index % 2 == 1) and ((word[0] == 'c') or (word[0] == 'e'))
-            if index % 2 == 1
-                unless word[0] == 'e'
-                    print "String " + word + " is invalid. New word: "
-                    new_string += gets.chomp.rstrip + ' '
-                end
+        corrected_string = ''
+        print "[" + (line + 1).to_s + "] Your string: "
+        initial_string = gets
+        initial_string.chomp.rstrip.split.each_with_index do | word, index |
+            corrected_string += word + ' ' unless (index % 2 == 1) and ((word[0] == 'c') or (word[0] == 'e'))
+            if index % 2 == 1 && word[0] == 'c'
+                print "Word " + word + " must be replaced. New word: "
+                corrected_string += gets.chomp.rstrip + ' '
             end
         end
-        result << new_string.rstrip
+        puts "Initial string: " + initial_string
+        puts "Corrected string: " + corrected_string
     end
-    return result
 end
 
-p input_and_correct_strings
+correct_strings
