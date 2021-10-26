9aff3b7fa3a77397de83cf895b8af45efd0e8d06
notblank00 <pyromancy00@gmail.com>
2021-10-26 09:22:25 +0300

    edit lab5

--- lab5.rb
+++ lab5.rb
@@ -1,24 +1,31 @@
-def calculate_expression x, y, z
-    (1 + Math.cos(y - 2)) / (x**4 / 2 + Math.sin(z)**2)
+# frozen_string_literal: true
+
+def calculate_expression(x, y, z)
+  (1 + Math.cos(y - 2)) / (x**4 / 2 + Math.sin(z)**2)
 end
 
-def correct_strings
-    print "Number of strings: "
-    n = gets.to_i
-    n.times do | line |
-        corrected_string = ''
-        print "[" + (line + 1).to_s + "] Your string: "
-        initial_string = gets
-        initial_string.chomp.rstrip.split.each_with_index do | word, index |
-            corrected_string += word + ' ' unless (index % 2 == 1) and ((word[0] == 'c') or (word[0] == 'e'))
-            if index % 2 == 1 && word[0] == 'c'
-                print "Word " + word + " must be replaced. New word: "
-                corrected_string += gets.chomp.rstrip + ' '
-            end
-        end
-        puts "Initial string: " + initial_string
-        puts "Corrected string: " + corrected_string
+def correct_string(initial_string)
+  corrected_string = ''
+  initial_string.chomp.rstrip.split.each_with_index do |word, index|
+    corrected_string += "#{word} " unless index.odd? && ((word[0] == 'c') | (word[0] == 'e'))
+    if index.odd? && word[0] == 'c'
+      print "Word #{word} must be replaced. New word: "
+      corrected_string += "#{gets.chomp.rstrip} "
     end
+  end
+  corrected_string
 end
 
-correct_strings
+def fetch_input
+  print 'Number of strings: '
+  n = gets.to_i
+  n.times do |line|
+    print "[#{line + 1}] Your string: "
+    initial_string = gets
+    corrected_string = correct_string initial_string
+    puts "Initial string: #{initial_string}"
+    puts "Corrected string: #{corrected_string}"
+  end
+end
+
+fetch_input
