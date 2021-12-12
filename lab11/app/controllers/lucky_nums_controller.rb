c25b1ab3465cf0764612682ff168b72acb0e7e28
notblank00 <pyromancy00@gmail.com>
2021-12-13 00:03:22 +0300

    vscode configs

--- lab11/app/controllers/lucky_nums_controller.rb
+++ lab11/app/controllers/lucky_nums_controller.rb
@@ -1,7 +1,34 @@
+# frozen-string-literal: true
+
+# Lucky Nums controller
 class LuckyNumsController < ApplicationController
-  def input
+  def input; end
+
+  def last_three(num)
+    num / 100 % 10 + num / 10 % 10 + num % 10
+  end
+
+  def first_three(num)
+    last_three(num / 1000)
+  end
+
+  def lucky?(num)
+    first_three(num) == last_three(num)
+  end
+
+  def lucky_numbers(range)
+    range.select { |num| lucky? num }
+         .map.with_index { |num, i| [i + 1, format('%06d', num), first_three(num)] }
   end
 
   def output
+    @from = params['from'].to_i || 0
+    @to = params['to'].to_i || 999_999
+    if (cached = LuckyNumsResult.find_by(from: @from, to: @to))
+      @rows = JSON.parse cached.result
+    else
+      @rows = lucky_numbers(@from..@to)
+      LuckyNumsResult.create from: @from, to: @to, result: JSON.unparse(@rows)
+    end
   end
 end
--- lab11/config/routes.rb
+++ lab11/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'lucky_nums/input'
-  get 'lucky_nums/output'
+  root 'lucky_nums#input'
+  match 'lucky_nums/output', via: %I[get post]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
